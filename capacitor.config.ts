7621f2b781ce5f8a30d9104c6083b40fdbbd7a1e
unknown <G00419414@atu.ie>
2024-04-23 12:18:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NativeLab',
+  webDir: 'www'
+};
+
+export default config;
